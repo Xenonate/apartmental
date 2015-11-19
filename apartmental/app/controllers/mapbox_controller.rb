cc36014e36b94da1cda001039ad9b8fda7028bfc
AakashL & Brian Rice <pair+aakashl.riceb53@devbootcamp.com>
2015-11-19 12:16:16 -0800

    Mapbox with pins, descriptions and photos and on the right half of the screen. Still trying to style better

---
+++ apartmental/app/controllers/mapbox_controller.rb
@@ -0,0 +1,9 @@
+class MapboxController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  # protect_from_forgery with: :exception
+
+  def playground
+  end
+
+end
