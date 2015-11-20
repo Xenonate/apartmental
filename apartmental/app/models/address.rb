599f519ddc9cc8bcae8bf24699755519a0a66b82
Mohamed Shibl & Brian Rice <pair+mshibl.riceb53@devbootcamp.com>
2015-11-20 12:13:24 -0800

    add hover option to maxbox markers

--- apartmental/app/models/address.rb
+++ apartmental/app/models/address.rb
@@ -1,26 +1,27 @@
 class Address < ActiveRecord::Base
-# def rental_average
-#   total_rent = 0
-#   if self.built < 1973
-#     for i in 1..length_of_stay
-#       total_rent += (self.price * (1.9**i)
-#     end
-#     total_rent
-#   else
-#     for i in 1..length_of_stay
-#       total_rent += (self.price * (1.9**i)
-#     end
-#     total_rent
-#   end
-# end
+def rental_average
+  total_rent = 0
+  if self.built.to_i < 1973
+    increase = 1.019
+  else
+    increase = 6
+  end
+     rent = 0
+  for i in 0..2 do
+     rent += 100*(increase ** i)
+  end
+    rent
+end
+
+def find_increase(neighborhood)
+  case neighborhood
+    when ""
+    when ""
+    end
+end
 
 
+  def total
 
-#   def total
-#     rent = 0
-#     for i in 0..2 do
-#      rent += 100*(1.1 ** i)
-#     end
-#       rent
-#   end
+  end
 end
