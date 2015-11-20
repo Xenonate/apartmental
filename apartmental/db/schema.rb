15d5195f085b0842d8e12f9122e3e2f01a387e0f
AakashL & Brian Rice <pair+aakashl.riceb53@devbootcamp.com>
2015-11-19 18:14:59 -0800

    add mapbox test view

--- apartmental/db/schema.rb
+++ apartmental/db/schema.rb
@@ -27,17 +27,17 @@ ActiveRecord::Schema.define(version: 20151119185923) do
     t.string "long"
   end
 
-  create_table "faves", force: :cascade do |t|
+  create_table "favorites", force: :cascade do |t|
     t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "user_faves", force: :cascade do |t|
+  create_table "user_favorites", force: :cascade do |t|
     t.integer  "user_id"
-    t.integer  "fave_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "favorite_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
