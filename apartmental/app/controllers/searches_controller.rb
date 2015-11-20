05461d6076f3995f33e8001e2b426de30958ff17
wenwei63029869 <email": null>
2015-11-19 22:24:04 -0800

    added homepage search form

--- apartmental/app/controllers/searches_controller.rb
+++ apartmental/app/controllers/searches_controller.rb
@@ -1,9 +1,24 @@
 class SearchesController < ActionController::Base
 
+  def create
+    @addresses = Address.where(city: search_params[:search_city], bedrooms: search_params[:search_bedroom]).where('price < ?', search_params[:search_price])
+    render 'search/index'
+  end
 
+  def index
+
+  end
+
+  private
+
+  def search_params
+    params.require(:search).permit(:search_price, :search_city, :search_bedroom, :search_period)
+  end
 
 end
 
 
 # var latitude = results[0].geometry.location.lat();
 # var longitude = results[0].geometry.location.lng();
+
+Address.where(city: "San Francisco", bedrooms: '1').where('price < ?', '2000')
