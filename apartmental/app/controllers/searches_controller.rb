0a9e7898e12476925f5d59e322c9ed942f3a1bb3
AakashL <email": null>
2015-11-22 16:34:05 -0800

    comment out google commute time search in controller

--- apartmental/app/controllers/searches_controller.rb
+++ apartmental/app/controllers/searches_controller.rb
@@ -19,18 +19,18 @@ class SearchesController < ActionController::Base
       @average_price = address.rental_average(@length_of_stay)
       @crime_rate = address.crime_rate
       coords = Geokit::Geocoders::GoogleGeocoder.geocode(search_params[:search_address])
-      call = "mode=#{search_params[:search_mode]}&origin=#{address.lat},#{address.long}&destination=#{coords.lat},#{coords.lng}"
-      url = URI.parse(URI.encode("http://api2.walkscore.com/api/v1/traveltime/json?wsapikey=b72221d8763203418d081f140357696e&#{call}"))
-      json = HTTParty.get(url)
-      response = JSON.parse(json.body)
+     # call = "mode=#{search_params[:search_mode]}&origin=#{address.lat},#{address.long}&destination=#{coords.lat},#{coords.lng}"
+     # url = URI.parse(URI.encode("http://api2.walkscore.com/api/v1/traveltime/json?wsapikey=b72221d8763203418d081f140357696e&#{call}"))
+     # json = HTTParty.get(url)
+     # response = JSON.parse(json.body)
       p "*" * 50
-      @commute_time = (response["response"]["results"][0]["travel_times"][0]["seconds"])/60 #in mins
+     # @commute_time = (response["response"]["results"][0]["travel_times"][0]["seconds"])/60 #in mins
 
       @walkscore = address.walkscore.to_i
 
       @price_range = (search_params[:search_max_price].to_i-search_params[:search_min_price].to_i)
 
-      @commute_time_range = (search_params[:search_max_time].to_i-search_params[:search_min_time].to_i)
+      #@commute_time_range = (search_params[:search_max_time].to_i-search_params[:search_min_time].to_i)
 
   #     get_weight(@price_weight, params[:price_weight])
   #     get_weight(@walkscore_weight, params[:walkscore_weight])
@@ -77,9 +77,9 @@ class SearchesController < ActionController::Base
       p "*" * 50
       p @pindex_crimerate = @crimescore * @crime_weight / 100
       p "*" * 50
-      p @pindex_commutescore = (@commutescore_weight/(@commute_time_range))*(search_params[:search_min_time].to_i-@commute_time)
+      #p @pindex_commutescore = (@commutescore_weight/(@commute_time_range))*(search_params[:search_min_time].to_i-@commute_time)
       p "pindex"
-      p @pindex = @pindex_price + @pindex_walkscore + @pindex_crimerate + @pindex_commutescore
+      p @pindex = @pindex_price + @pindex_walkscore + @pindex_crimerate #+ @pindex_commutescore
 
       @sorted_results[@pindex] = address
 
