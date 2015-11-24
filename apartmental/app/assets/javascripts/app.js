2d63b2fd598d2ca633f51babd19c7af6ddca790b
wenwei63029869 <email": null>
2015-11-24 00:26:13 -0800

    be able to load polygon on click

--- apartmental/app/assets/javascripts/app.js
+++ apartmental/app/assets/javascripts/app.js
@@ -1,3 +1,88 @@
 $( document ).ready(function() {
+  $(".search_form").on("submit", function(){
+    $(this).serialize();
+
+  });
+
+  $(".clickable_item").on("click", function(e){
+    e.preventDefault();
+    var id = $(this).attr("id")
+    var search_id = $(this).attr("data")
+    console.log(id)
+    var myData = {
+      "address_id": id,
+      "search_id": search_id
+    }
+    var s="";
+    for(var prop in myData){
+      if(s.length===0){
+        s+=prop+"="+myData[prop]
+      }else{
+        s+="&"+prop+"="+myData[prop]
+      }
+    }
+
+    $.ajax({
+      url: "/charts",
+      method: "GET",
+      data: s,
+      dataType: 'json'
+    })
+    .done(function(msg){
+      // console.log(msg)
+      // console.log(msg)
+      console.log([
+            parseFloat(msg[0].pindex_price),
+            parseFloat(msg[0].pindex_commutescore),
+            parseFloat(msg[0].pindex_crimerate),
+            parseFloat(msg[0].pindex_walkscore)
+          ])
+      var ctx = $("#myChart").get(0).getContext("2d");
+      var data = {
+        labels: ["Rent", "Commute Time", "Crime Rate", "Walkscore"],
+        datasets: [
+        {
+          label: "perfect weight",
+          fillColor: "rgba(220,220,220,0.2)",
+          strokeColor: "rgba(220,220,220,1)",
+          pointColor: "rgba(220,220,220,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data: [
+            parseFloat(msg[0].price_weight),
+            parseFloat(msg[0].commute_weight),
+            parseFloat(msg[0].crime_weight),
+            parseFloat(msg[0].walkscore_weight)
+          ]
+        },
+        {
+          label: "actual weight",
+          fillColor: "rgba(151,187,205,0.2)",
+          strokeColor: "rgba(151,187,205,1)",
+          pointColor: "rgba(151,187,205,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(151,187,205,1)",
+          data: [
+            parseFloat(msg[0].pindex_price),
+            parseFloat(msg[0].pindex_commutescore),
+            parseFloat(msg[0].pindex_crimerate),
+            parseFloat(msg[0].pindex_walkscore)
+          ]
+        }
+        ]
+      };
+
+      console.log(data.datasets)
+
+      var myRadarChart = new Chart(ctx).Radar(data);
+      console.log(myRadarChart)
+      $("#myChart").append(myRadarChart)
+    })
+    .fail(function(error){
+      console.log(error.responseText);
+    })
+});
 
 });
--- apartmental/app/controllers/searches_controller.rb
+++ apartmental/app/controllers/searches_controller.rb
@@ -3,136 +3,106 @@ class SearchesController < ActionController::Base
   def create
     # @addresses = Address.where(city: search_params[:search_city])
     @addresses = Address.where(city: search_params[:search_city], bedrooms: search_params[:search_bedroom]).where('price > ?', search_params[:search_min_price]).where('price < ?', search_params[:search_max_price])
-
+    @search= Search.create
     @long_lat_link = []
     @addresses.each do |address|
       @long_lat_link << [[address.long.to_f, address.lat.to_f], address.listing_url]
     end
     @length_of_stay = search_params[:search_period] != "" ? search_params[:search_period].to_i : 1
 # /////////////////////////////////////////////////////////////////////////////
-@sorted_results = {}
-@addresses.each do |address|
-      # p address
-      # p address.id
-      # p address.price.to_i
-      # p "average_price"
-      @average_price = address.rental_average(@length_of_stay)
-      @crime_rate = address.crime_rate
+  @sorted_results = {}
+  @addresses.each do |address|
+        # p address
+        # p address.id
+        # p address.price.to_i
+        # p "average_price"
+        @average_price = address.rental_average(@length_of_stay)
+        @crime_rate = address.crime_rate
 
-      #////Commented out Google API calls //////#
+        #////Commented out Google API calls //////#
 
-      # coords = Geokit::Geocoders::GoogleGeocoder.geocode(search_params[:search_address])
-      #call = "mode=#{search_params[:search_mode]}&origin=#{address.lat},#{address.long}&destination=#{coords.lat},#{coords.lng}"
-      call = "mode=#{search_params[:search_mode]}&origin=#{address.lat},#{address.long}&destination=37.7706312,-122.4167"
-      url = URI.parse(URI.encode("http://api2.walkscore.com/api/v1/traveltime/json?wsapikey=b72221d8763203418d081f140357696e&#{call}"))
-      json = HTTParty.get(url)
-      response = JSON.parse(json.body)
-      p "*" * 8
-      p response
-      @commute_time = (response["response"]["results"][0]["travel_times"][0]["seconds"])/60 #in mins
+        # coords = Geokit::Geocoders::GoogleGeocoder.geocode(search_params[:search_address])
+        #call = "mode=#{search_params[:search_mode]}&origin=#{address.lat},#{address.long}&destination=#{coords.lat},#{coords.lng}"
+        call = "mode=#{search_params[:search_mode]}&origin=#{address.lat},#{address.long}&destination=37.7706312,-122.4167"
+        url = URI.parse(URI.encode("http://api2.walkscore.com/api/v1/traveltime/json?wsapikey=b72221d8763203418d081f140357696e&#{call}"))
+        json = HTTParty.get(url)
+        response = JSON.parse(json.body)
+        p "*" * 8
+        p response
+        @commute_time = (response["response"]["results"][0]["travel_times"][0]["seconds"])/60 #in mins
 
-      @walkscore = address.walkscore.to_i
+        @walkscore = address.walkscore.to_i
 
-      @price_range = (search_params[:search_max_price].to_i-search_params[:search_min_price].to_i)
+        @price_range = (search_params[:search_max_price].to_i-search_params[:search_min_price].to_i)
 
-      @commute_time_range = (search_params[:search_max_time].to_i-search_params[:search_min_time].to_i)
-
-
-      # def get_weight(type, param)
-      #   if param
-      #     type = param
-      #   else
-      #     type = 0.25
-      #   end
-      # end
-
-      # get_weight(@price_weight, params[:price_weight])
-      # get_weight(@walkscore_weight, search_params[:walkscore_weight])
-      # get_weight(@crime_weight, search_params[:crime_weight])
-      # get_weight(@commutescore_weight, search_params[:commutescore])
-
-      p @price_weight = search_params[:price_weight].to_i
-      p @commute_weight = search_params[:commute_weight].to_i
-      p @walkscore_weight = search_params[:walkscore_weight].to_i
-      p @crime_weight = search_params[:crime_weight].to_i
+        @commute_time_range = (search_params[:search_max_time].to_i-search_params[:search_min_time].to_i)
 
 
-      @total_weight = @price_weight + @commute_weight + @walkscore_weight + @crime_weight
+        # def get_weight(type, param)
+        #   if param
+        #     type = param
+        #   else
+        #     type = 0.25
+        #   end
+        # end
 
-      case @crime_rate
-      when "A"
-        @crimescore = 95
-      when "B"
-        @crimescore = 85
-      when "C"
-        @crimescore = 75
+        # get_weight(@price_weight, params[:price_weight])
+        # get_weight(@walkscore_weight, search_params[:walkscore_weight])
+        # get_weight(@crime_weight, search_params[:crime_weight])
+        # get_weight(@commutescore_weight, search_params[:commutescore])
+
+        p @price_weight = search_params[:price_weight].to_i
+        p @commute_weight = search_params[:commute_weight].to_i
+        p @walkscore_weight = search_params[:walkscore_weight].to_i
+        p @crime_weight = search_params[:crime_weight].to_i
+
+
+        @total_weight = @price_weight + @commute_weight + @walkscore_weight + @crime_weight
+
+        case @crime_rate
+        when "A"
+          @crimescore = 95
+        when "B"
+          @crimescore = 85
+        when "C"
+          @crimescore = 75
+        end
+
+          # p "price weight"
+          # p (@price_weight.to_f/@total_weight.to_f)
+          # p "total weight"
+          # p (@average_price - search_params[:search_min_price].to_i)/(@price_range)
+          # p "average price"
+          # p @average_price
+          # p search_params[:search_min_price].to_i
+          # p "price range"
+          # p @price_range
+          # p "pindex price"
+
+          @pindex_price = (@price_weight.to_f/@total_weight.to_f)*(search_params[:search_max_price].to_f - @average_price.to_f)/(@price_range.to_f) * 100
+          "*" * 50
+          @pindex_walkscore = @walkscore.to_f * @walkscore_weight.to_f / @total_weight.to_f
+          "*" * 50
+          @pindex_crimerate = @crimescore.to_f * @crime_weight.to_f / @total_weight.to_f
+          "*" * 50
+
+          @pindex_commutescore = (@commute_weight.to_f / @total_weight.to_f)*(search_params[:search_max_time].to_f-@commute_time / @commute_time_range.to_f)
+
+          p @pindex = @pindex_price + @pindex_walkscore + @pindex_crimerate + @pindex_commutescore
+
+
+
+        SearchResult.create(search_id: @search.id, address_id: address.id, pindex:@pindex, pindex_price: @pindex_price, pindex_walkscore: @pindex_walkscore, pindex_commutescore: @pindex_commutescore, pindex_crimerate: @pindex_crimerate, price_weight: @price_weight, commute_weight: @commute_weight, walkscore_weight: @walkscore_weight, crime_weight:@crime_weight)
+
+          @sorted_results[@pindex] = {
+            "address" => address,
+            "search_id" => @search.id
+        }
+
       end
-
-        # p "price weight"
-        # p (@price_weight.to_f/@total_weight.to_f)
-        # p "total weight"
-        # p (@average_price - search_params[:search_min_price].to_i)/(@price_range)
-        # p "average price"
-        # p @average_price
-        # p search_params[:search_min_price].to_i
-        # p "price range"
-        # p @price_range
-        # p "pindex price"
-
-       @pindex_price = (@price_weight.to_f/@total_weight.to_f)*(search_params[:search_max_price].to_f - @average_price.to_f)/(@price_range.to_f) * 100
-       "*" * 50
-       @pindex_walkscore = @walkscore.to_f * @walkscore_weight.to_f / @total_weight.to_f
-       "*" * 50
-       @pindex_crimerate = @crimescore.to_f * @crime_weight.to_f / @total_weight.to_f
-       "*" * 50
-
-       @pindex_commutescore = (@commute_weight.to_f / @total_weight.to_f)*(search_params[:search_max_time].to_f-@commute_time / @commute_time_range.to_f)
-
-      p @pindex = @pindex_price + @pindex_walkscore + @pindex_crimerate + @pindex_commutescore
-
-
-
-
-      @sorted_results[@pindex] = {
-        "address" => address,
-        "pindex_price" => @pindex_price,
-        "pindex_walkscore" => @pindex_walkscore,
-        "pindex_commutescore" => @pindex_walkscore,
-        "pindex_crimerate" => @pindex_crimerate,
-        "price_weight" => (@price_weight/@total_weight.to_f)*100,
-        "commute_weight" => (@commute_weight/@total_weight.to_f)*100,
-        "walkscore_weight" => (@walkscore_weight/@total_weight.to_f)*100,
-        "crime_weight" => (@crime_weight/@total_weight.to_f)*100
-        # "price_weight" => (@price_weight/@total_weight)*100,
-        # "commute_weight" => (@commute_weight/@total_weight)*100,
-        # "walkscore_weight" => (@walkscore_weight/@total_weight)*100,
-        # "crime_weight" => (@crime_weight/@total_weight)*100
-      }
-
-      # p "@walkscore.to_f"
-      # p @walkscore.to_f
-      # p "@walkscore_weight.to_f"
-      # p @walkscore_weight.to_f
-      # p "@total_weight.to_f"
-      # p @total_weight.to_f
-      # p @pindex_walkscore
-
-      # p "@crimescore.to_f"
-      # p @crimescore.to_f
-      # p "@crime_weight.to_f"
-      # p @crime_weight.to_f
-      # p "@total_weight.to_f"
-      # p @total_weight.to_f
-      # p @pindex_crimerate
-
-
-    end
 
     @sorted_results.sort_by{|key, value| key}
     p "*" * 50
-    # @sorted_results.each_value do |value|
-    #     p value
-    #   end
 
     render 'searches/index', layout: 'layouts/application'
   end
