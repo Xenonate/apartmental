c10513a1a6cd797be08fc2e8cdd4a899e48920e6
wenwei63029869 <email": null>
2015-11-24 17:46:06 -0800

    finish ajax mapbox

--- apartmental/app/assets/javascripts/application.js
+++ apartmental/app/assets/javascripts/application.js
@@ -14,21 +14,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require homepage
 //= require app
+//= require apmapbox
 //= require mapbox
 //= require foundation
 //= require_tree
 //= require searches
 
-
 $(document).foundation();
-$(document).on('ready', function(){
-  $('#options').hide()
-  $('#options-button').on('click', function(e){
-    e.preventDefault();
-    $('#options').toggle()
-  })
-  // console.log('Derek said to console log something right here')
-  // console.log(document.querySelector('.options-open'))
 
-})
+
